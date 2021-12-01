0a0f6d3bbb5ed65d7a8c45087f24e851cd693705
aiyc <1432803776@qq.com>
2021-12-01 19:39:04 +0800

    update

--- js/1.js
+++ js/1.js
@@ -1,5 +1,5 @@
-	// var shijian=120;
-	var shijian=10;
+	var shijian=120;
+	// var shijian=2;
 	var time=null;
 	function start(){
 		 time=setInterval("count()",1000);//倒计时
@@ -9,7 +9,7 @@
 		const audio = new Audio('src/tkd.mp3');
 		if(shijian<0){
 			audio.play();
-			shijian=10;
+			shijian=120;
 			// alert("时间到！");
 			pause();//一般情况下这个 if 条件是用来提交数据用的
 		}else{
@@ -23,4 +23,31 @@
 	}
 	function goOn(){
 		time = setInterval("count()",1000);
+	}
+// ----------------------------------
+	// var shijian2=120;
+	var shijian2=3;
+	var time2=null;
+	function start2(){
+		time2=setInterval("count2()",1000);//倒计时
+	}
+
+	function count2(){
+		const audio2 = new Audio('src/tkd.mp3');
+		if(shijian2<0){
+			audio2.play();
+			shijian2=10;
+			// alert("时间到！");
+			pause2();//一般情况下这个 if 条件是用来提交数据用的
+		}else{
+			$("#time2 p.timedown2").html(Math.floor(shijian2/60)+":"+shijian2%60);
+			shijian2--;
+			console.info(time2);
+		}
+	}
+	function pause2(){
+		clearInterval(time2);
+	}
+	function goOn2(){
+		time2 = setInterval("count2()",1000);
 	}
