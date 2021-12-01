908f84abb6592774fa2f2db82aeebf825ac93077
aiyc <1432803776@qq.com>
2021-12-01 08:50:17 +0800

    update

--- js/1.js
+++ js/1.js
@@ -1,13 +1,17 @@
-	var shijian=120;
+	// var shijian=120;
+	var shijian=10;
 	var time=null;
 	function start(){
 		 time=setInterval("count()",1000);//倒计时
 	}
 	
 	function count(){
+		const audio = new Audio('src/tkd.mp3');
 		if(shijian<0){
+			audio.play();
 			alert("time out");
-			pause();//一般情况下这个if条件是用来提交数据用的
+			shijian=10;
+			pause();//一般情况下这个 if 条件是用来提交数据用的
 		}else{
 			$("#time p").html(Math.floor(shijian/60)+":"+shijian%60);
 			shijian--;
