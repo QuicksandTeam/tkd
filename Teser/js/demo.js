1187fa773a5cd04826214e3b0277fd273c37ebdb
aiyc <1432803776@qq.com>
2021-12-03 21:39:37 +0800

    update

--- Teser/js/demo.js
+++ Teser/js/demo.js
@@ -1,10 +1,13 @@
-var  i = 0;
+var i = 0;
 
-function appendSomeItems() {
-    for (var j=0; j<50; i++, j++) {
-        $('#collection').append('<li>Line Item</li>');
-    }
-    if (i < 5000) window.setTimeout(appendSomeItems, 0);
+function appendPoint() {
+    // for (var j=0; j<50; i++, j++) {
+    $('#collection').append('●<br>');
+    // }
+    // if (i < 5000) window.setTimeout(appendSomeItems, 0);
 }
-
+function delPoint() {
+    // $('#collection li').remove(0);
+    $('#collection li').remove('<li>Line Item</li>');
+}
 // appendSomeItems();
